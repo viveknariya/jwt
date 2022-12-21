73808d521267de793929fdeb9335b20f6cd68863
vivek <vhnariya@gmail.com>
2022-12-21 14:07:00 +0530

    first

---
+++ jwtAuthentication/Controllers/UserController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace jwtAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration) {
+
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+
+        public async Task<ActionResult<User>> register(UserDto userDto)
+        {
+            createPasswordHash(userDto.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Username= userDto.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+
+            return Ok(user);
+        }
+
+        private void createPasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA256())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> login(UserDto userDto)
+        {
+            if(userDto.Username != user.Username)
+            {
+                return NotFound("user not found.");
+            }
+            if (!verifyPasswordHash(userDto.Password,user.PasswordHash,user.PasswordSalt))
+            {
+                return BadRequest("wrong password");
+            }
+
+            var token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "Noob")
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("Key:SecurityKey").Value));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims:claims,
+                expires:DateTime.Now.AddDays(1),
+                signingCredentials:cred
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+
+        }
+
+        private bool verifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA256(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);    
+            }
+        }
+
+        
+    }
+}
---
+++ jwtAuthentication/User.cs
@@ -0,0 +1,11 @@
+﻿namespace jwtAuthentication
+{
+    public class User
+    {
+        public string Username { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
